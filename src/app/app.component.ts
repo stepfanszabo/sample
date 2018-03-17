db59af570990f96bdb6b2c685029249209a7e642
Stefan <szstefan137@gmail.com>
2018-03-17 20:22:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tiles = [
+    {text: 'Trip 1', location: "Rijeka, Croatia", date: "July 2017"},
+    {text: 'Trip 2', location: "Skopje, Macedonia", date: "August 2017"},
+    {text: 'Trip 3', location: "Sarajevo, Bosnia", date: "August 2017"},
+    {text: 'Trip 4', location: "Bled, Slovenia", date: "September 2017"},
+    {text: 'Trip 5', location: "Rome, Italy", date: "October 2017"}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material'; 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
